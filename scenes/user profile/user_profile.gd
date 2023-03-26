ff5d4c8bd09bc75b179ed491c5d025902b75f294
pawarherschel <pawarherschel@gmail.com>
2023-03-26 23:26:19 +0530

    updating the value in singleton

--- scenes/user profile/user_profile.gd
+++ scenes/user profile/user_profile.gd
@@ -9,11 +9,11 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/Username.text = Singleton.username if Singleton.username else "username"
-	$VBoxContainer/OptionButton.add_item("dance")
-	$VBoxContainer/OptionButton.add_item("art")
-	$VBoxContainer/OptionButton.add_item("music")
-	$VBoxContainer/OptionButton.add_item("other")
-	$VBoxContainer/OptionButton.add_item("mix")
+	$VBoxContainer/OptionButton.add_item("mix", 1)
+	$VBoxContainer/OptionButton.add_item("dance", 2)
+	$VBoxContainer/OptionButton.add_item("art", 3)
+	$VBoxContainer/OptionButton.add_item("music", 4)
+	$VBoxContainer/OptionButton.add_item("other", 5)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,4 +26,17 @@ func _on_back_pressed():
 		show_progress_bar = true,
 	}
 	Game.change_scene("res://scenes/relaxation activity/relaxation_activity.tscn", params)
+
+
+func _on_OptionButton_item_selected(index):
+	if index == 1:
+		Singleton.hobby = "mix"
+	if index == 2:
+		Singleton.hobby = "dance"
+	if index == 3:
+		Singleton.hobby = "art"
+	if index == 4:
+		Singleton.hobby = "music"
+	if index == 5:
+		Singleton.hobby = "other"
 	
